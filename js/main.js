544f217c2afdd30e880dcbc2ba23ab48ee5cb29a
Arun Kumar <92267548+arunkmr129@users.noreply.github.com>
2025-05-04 01:32:11 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function loadPage(page) {
+    fetch(page)
+      .then(response => {
+        if (!response.ok) throw new Error('Page not found');
+        return response.text();
+      })
+      .then(html => {
+        document.getElementById('content').innerHTML = html;
+      })
+      .catch(error => {
+        document.getElementById('content').innerHTML = `<p>Error loading page: ${error.message}</p>`;
+      });
+  }
+  
